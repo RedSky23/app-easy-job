18ddc4d2e2a3745d07e472e4877e5d3a65ed57c2
luis_ <luis.bendez342@gmail.com>
2023-11-20 20:31:56 -0500

    app.module changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,24 +25,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
 import { WorkerComponent } from './component/worker/worker.component';
 import { ListarComponent } from './component/worker/listar/listar.component';
 import { WorkerCreateEditComponent } from './component/worker/worker-create-edit/worker-create-edit.component';
 import { CreateServiceContractComponent } from './employeer/create-service-contract/create-service-contract.component';
-=======
-import { WorkerComponent } from './worker/worker.component';
-import { ListarComponent } from './worker/listar/listar.component';
-
-
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableModule } from '@angular/material/table';
-import { DialogoComponent } from './worker/listar/dialogo/dialogo.component';
-import {MatDialogModule} from '@angular/material/dialog';
-import { WorkerCreateEditComponent } from './worker/worker-create-edit/worker-create-edit.component';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-
->>>>>>> 8e2fb2051b879132ef714f1f68dc34c2af9eff67
 
 
 @NgModule({
@@ -53,13 +39,8 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
     CreateEditComponent,
     WorkerComponent,
     ListarComponent,
-<<<<<<< HEAD
     WorkerCreateEditComponent,
     CreateServiceContractComponent
-=======
-    DialogoComponent,
-    WorkerCreateEditComponent
->>>>>>> 8e2fb2051b879132ef714f1f68dc34c2af9eff67
   ],
   imports: [
     BrowserModule,
